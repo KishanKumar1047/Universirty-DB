267da120ec43cbc1833d01cbe1b6c6ee8e059ea7
Kishan Kumar <166985062+KishanKumar1047@users.noreply.github.com>
2026-02-13 00:13:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def get_connection():
     return mysql.connector.connect(
         host="127.0.0.1",
         user="root",
-        password="123456789", #123456789
+        password="", #
         database="university_db"
     )
 # ---------- LOGIN LOGIC ----------
